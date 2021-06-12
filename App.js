d9432d5023429d5a1177cb5f5b29fc784a9a3659
William Phokompe <phokompe.william@outlook.com>
2021-06-12 19:55:49 +0200

    control how notifications are displayed

--- App.js
+++ App.js
@@ -1,23 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 import * as Notifications from "expo-notifications";
-import * as Permissions from 'expo-permissions'
+import * as Permissions from "expo-permissions";
+
+Notifications.setNotificationHandler({
+  handleNotification: async (_) => {
+    return {
+      shouldShowAlert: true,
+    };
+  },
+});
 
 export default function App() {
-
-  useEffect(_ => {
-    Permissions.getAsync(Permissions.NOTIFICATIONS).then(statusObj => {
-      if (statusObj.status !== 'granted') {
-        return Permissions.askAsync(Permissions.NOTIFICATIONS)
-      }
-      return statusObj;
-    }).then(statusObj => {
-      if (statusObj.status !== 'granted') {
-        return ;
-      }
-    })
-  }, [])
+  useEffect((_) => {
+    Permissions.getAsync(Permissions.NOTIFICATIONS)
+      .then((statusObj) => {
+        if (statusObj.status !== "granted") {
+          return Permissions.askAsync(Permissions.NOTIFICATIONS);
+        }
+        return statusObj;
+      })
+      .then((statusObj) => {
+        if (statusObj.status !== "granted") {
+          return;
+        }
+      });
+  }, []);
 
   const triggerNotificationHandler = (_) => {
     Notifications.scheduleNotificationAsync({
